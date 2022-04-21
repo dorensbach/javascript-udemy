256ba978a26cf02702ba3c8e686cda7784f6b11b
Paulo Dorensbach <paulo.dorensbach@quantosobra.com.br>
2022-04-21 15:41:43 -0300

    Aula 09

---
+++ aula09/index.js
@@ -0,0 +1,13 @@
+// aula 09
+// let x var
+// 21.04.2022
+
+// Var permite redeclarar a variável
+var nome = 'Luiz';
+console.log(nome);
+var nome = 'Abel';
+console.log(nome);
+
+// Não criar variaveis sem let
+nomeVar = 'Bruno';
+console.log(nomeVar);
