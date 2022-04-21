5b1404e59db6e034642021200549e6a0f58ea99d
Paulo Dorensbach <paulo.dorensbach@quantosobra.com.br>
2022-04-21 13:35:51 -0300

    Aula 3

---
+++ aula3/index.js
@@ -0,0 +1,13 @@
+// aula 03 - index.js
+// console.log()
+
+console.log('Paulo "teste"');
+console.log("Paulo 'teste'");
+console.log(`'Teste' "2"`);
+
+console.log(56);
+console.log(10.78);
+
+console.log(26, 44.88, 'Teste', 'Teste 2', 666, 1.58);
+
+console.log('Meu nome é "PAULO". Estou aprendendo Javascript às', 10, 'da manhã.')
