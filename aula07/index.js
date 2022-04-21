0050ca31ef55c356f3bdf8c4ae341ad7f65c250b
Paulo Dorensbach <paulo.dorensbach@quantosobra.com.br>
2022-04-21 15:23:39 -0300

    Aula 07

---
+++ aula07/index.js
@@ -0,0 +1,20 @@
+// aula 07
+// constantes com const e tupeof
+// 21.04.2022
+
+// const precisa ser inicializada na criação
+
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+let conta = primeiroNumero + segundoNumero;
+console.log(conta);
+
+console.log(typeof(primeiroNumero));
+console.log(typeof(nome));
+
+const stringNumero = '5';
+console.log(stringNumero + segundoNumero);
+console.log(typeof(stringNumero + segundoNumero));
