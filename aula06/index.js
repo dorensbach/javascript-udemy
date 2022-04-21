b2f0874345315dd505d2547f32719d269a5a1995
Paulo Dorensbach <paulo.dorensbach@quantosobra.com.br>
2022-04-21 14:45:45 -0300

    Aula 06

---
+++ aula06/index.js
@@ -0,0 +1,15 @@
+// aula 06
+// variaveis com let
+// 21.04.2022
+
+// var não é mais utilizado. Código mais antigo
+
+// Usa-se let para variáveis
+let nome = 'João';
+console.log(nome, 'Pedro.');
+
+let nome2;
+console.log(nome2);
+
+nome2 = 'Carlos';
+console.log(nome2);
