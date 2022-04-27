568cf291f1850e4319d6e0ec762e9c88f13ddc1a
Paulo Dorensbach <dorensbach@gmail.com>
2022-04-27 08:35:02 -0300

    Aula 22 - Operadores logicos e de comparacao

---
+++ aula22/index.js
@@ -0,0 +1,21 @@
+// aula 22
+// operadores de comparacao e logicos
+// 27.04.2022
+
+// Não se usa a comparação apenas de valor (== e !=)
+/**
+ * Comparação
+ * >
+ * >=
+ * <
+ * <=
+ * ===
+ * !==
+ */
+
+/**
+ * Operadores lógicos
+ * && = E
+ * || = OU
+ * !  = NOT
+ */
