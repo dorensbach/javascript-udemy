ee0411808b13cf7039c0d8f7cdc9928da3a14129
Paulo Dorensbach <dorensbach@gmail.com>
2022-05-10 13:02:28 -0300

    Aula 40 - Até 37 min

--- aula40/assets/js/main.js
+++ aula40/assets/js/main.js
@@ -1,6 +1,6 @@
 // aula 40
 // lista de tarefas
-// 09.05.2022
+// 09.05.2022 - 10.05.2022
 
 const imputTarefa = document.querySelector('.imput-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
@@ -10,17 +10,62 @@ function criaLi() {
     return document.createElement('li');
 };
 
+function limpaImput() {
+    imputTarefa.value = '';
+    imputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    //botaoApagar.classList.add('classe-do-botao'); // Outra maneira de adicionar classe a um elemento
+    // setAtribute: usado para setar qualquer atributo para o elemento
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerHTML = textoInput;
     tarefas.appendChild(li);
+    limpaImput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
 }
 
 imputTarefa.addEventListener('keypress', function(e) {
-    console.log(e);
+    if (e.keyCode === 13) {
+        if (!imputTarefa.value) return
+        criaTarefa(imputTarefa.value);
+    }
 });
 
 btnTarefa.addEventListener('click', function() {
     if (!imputTarefa.value) return
     criaTarefa(imputTarefa.value);
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    // Printa qual elemento foi clicado
+    // console.log(el);
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
 });
