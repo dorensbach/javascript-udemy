3d1745a6a4bd89316ca5b54018779c7dc40d18e8
Paulo Dorensbach <dorensbach@gmail.com>
2022-05-16 12:28:26 -0300

    Timer Café

--- timer-cafe/assets/js/main.js
+++ timer-cafe/assets/js/main.js
@@ -6,6 +6,7 @@ const contador = document.querySelector('.contador');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+const icone = document.querySelector('.icone');
 
 let count = 0;
 
@@ -15,12 +16,21 @@ function formataDoisDig(num) {
     return num < 10 ? '0' + num.toString() : num.toString();
 }
 
-function montaTimer() {
+function montaTimer(count) {
     let segundos = count % 60;;
     let minutos = Math.floor(count/60);
     minutos = minutos < 60 ? minutos :minutos % 60;
     let horas = Math.floor((count/60)/60)
     return `${formataDoisDig(horas)}:${formataDoisDig(minutos)}:${formataDoisDig(segundos)}`;
+}
+
+function setaIcone(count) {
+    if (count < 300) return '<img src="./assets/img/face-laugh-beam.svg">';
+    if (count < 600) return '<img src="./assets/img/face-frown.svg">';
+    if (count < 750) return '<img src="./assets/img/face-sad-tear.svg">';
+    if (count < 900) return '<img src="./assets/img/face-sad-cry.svg">';
+    if (count >= 900) return '<img src="./assets/img/face-angry.svg">';
+
 }
 
 iniciar.addEventListener('click', function(event) {
@@ -29,6 +39,7 @@ iniciar.addEventListener('click', function(event) {
     timer = setInterval(function (){
         count++;
         contador.innerHTML = montaTimer(count);
+        icone.innerHTML = setaIcone(count);
     }, 1000);
 });
 
@@ -41,5 +52,6 @@ zerar.addEventListener('click', function (event) {
     clearInterval(timer);
     contador.classList.remove('pausado');
     count = 0;
-    contador.innerHTML = montaTimer();
+    contador.innerHTML = montaTimer(count);
+    icone.innerHTML = setaIcone(count);
 })
