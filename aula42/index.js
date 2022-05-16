3695ab0de0673a21a791b1f1ba80bdb988e49de6
Paulo Dorensbach <dorensbach@gmail.com>
2022-05-16 08:57:41 -0300

    Aula 42 - Parâmetros de funções

--- aula42/index.js
+++ aula42/index.js
@@ -1,6 +1,6 @@
 // aula 42
 // parametros de funcoes
-// 13.05.2022
+// 13.05.2022 - 16.05.2022
 
 /**
  * Funções definidas com a palavra function, tem o argumento especial Arguments com todos argumentos enviados
@@ -11,11 +11,15 @@ function funcao1() {
 }
 funcao1('Texto', 0, 12, 'casa', 'bola', true, 7);
 
+console.log('###############################')
+
 function funcao2(a, b, c) {
     console.log(arguments);
     console.log(arguments[6]);
 }
 funcao2('Texto', 0, 12, 'casa', 'bola', true, 7);
+
+console.log('###############################')
 
 function funcao3() {
     let total = 0;
@@ -26,3 +30,55 @@ function funcao3() {
     console.log(total);
 }
 funcao3(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+console.log('###############################')
+
+function funcao4(a, b, c, d, e, f, g) {
+    console.log(a, b, c, d, e, f, g);
+}
+funcao4(1, 2, 3);
+
+console.log('###############################')
+
+function funcao5(a, b = 2, c = 4) {
+    console.log(a + b + c);
+}
+funcao5(1, 10);
+
+console.log('###############################')
+
+/**
+ * Para forçar a função a usar o valor padrão, passar o parâmetro como undefined
+ */
+function funcao6(a, b = 2, c = 4) {
+    console.log(a + b + c);
+}
+funcao6(1, undefined, 10);
+
+console.log('###############################')
+
+function funcao7({ nome, sobrenome, idade}) {
+    console.log(nome, sobrenome, idade);
+}
+funcao7({
+    nome: 'Lucas',
+    sobrenome: 'Silva e Silva',
+    idade: '35'
+});
+
+console.log('###############################')
+
+function funcao8([valor1, valor2, valor3]) {
+    console.log(valor1, valor2, valor3);
+}
+funcao8(['Lucas','Silva e Silva','33']);
+
+console.log('###############################')
+
+// Rest operator. Precisa ser o último parâmetro da função
+function funcao9(nome, sobrenome, ...numeros) {
+    console.log(nome, sobrenome, numeros);
+}
+funcao9('Lucas','Silva e Silva', 1, 7, 15, 33, 45, 58);
+
+console.log('###############################')
