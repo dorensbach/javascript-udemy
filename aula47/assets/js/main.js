f057042b8e56512c0865786fddd452f958084c98
Paulo Dorensbach <dorensbach@gmail.com>
2022-05-25 12:57:42 -0300

    Aula 47 - Calculadora Factory Functions

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -1,3 +1,44 @@
 // aula 47
 // Calculadora - Praticando factory functions
 // 24.05.2022 -
+
+/**
+ * Arrow functions não mudam o comportamento do this
+ * .bind(this) = "corrige" o this em uma function chamada por outro elemento em um objeto
+ */
+
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+        clear: document.querySelector('.btn-clear'),
+
+        inicia() {
+            this.cliqueBotoes();
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        },
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
