8e5078ef1a0e4516cd9d3283c2c094f0f9dc0a06
Paulo Dorensbach <dorensbach@gmail.com>
2024-06-13 23:16:47 -0300

    Aula 47 - Calculadora Factory Functions - Final
    Signed-off-by: Paulo Dorensbach <dorensbach@gmail.com>

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -1,11 +1,6 @@
 // aula 47
 // Calculadora - Praticando factory functions
-// 24.05.2022 -
-
-/**
- * Arrow functions não mudam o comportamento do this
- * .bind(this) = "corrige" o this em uma function chamada por outro elemento em um objeto
- */
+// 24.05.2022 - 13.06.2024
 
 function criaCalculadora() {
     return {
@@ -14,13 +9,51 @@ function criaCalculadora() {
 
         inicia() {
             this.cliqueBotoes();
+            this.pressionaEnter();
         },
 
         clearDisplay() {
             this.display.value = '';
         },
 
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                // Não se deve usar eval, mas para fins didáticos, está sendo usado
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta inválida');
+                return;
+            }
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
         cliqueBotoes() {
+            /**
+             * Arrow functions não mudam o comportamento do this,
+             * se usar function(){}, o this vai se referir ao document
+             * .bind(this) = "corrige" o this em uma function chamada por outro elemento em um objeto
+             * o .bind vai no final da function
+             */
             document.addEventListener('click', e => {
                 const el = e.target;
 
@@ -30,6 +63,14 @@ function criaCalculadora() {
 
                 if (el.classList.contains('btn-clear')) {
                     this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                 }
             });
         },
